3c98d0b00b9f12e45b3678cd97d70ba5fd70fc32
adug3 <m_fan@geishatokyo.com>
2018-11-10 20:35:28 +0900

    add basic page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import Button from 'antd/lib/button';
+import { Layout, Menu, Spin, Alert } from 'antd';
 import './App.css';
 
+const { Header, Content, Footer } = Layout;
 class App extends Component {
+
+  renderContent = () => {
+    return "textfjadlfkajfhalkjdhfalkfhakjfdhaklhfladjhkflahflk"
+  }
+
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+      <Layout>
+        <Header className="header">
+          <div className="logo">Blog</div>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['employer']}
+            style={{ lineHeight: '64px' }}
+            onSelect={this.onSelectTab}
+          >
+            <Menu.Item key="1">text1</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Layout style={{ padding: '24px 0', background: '#fff', minHeight: '600px' }}>
+            {this.renderContent()}
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Dorahackathon ©2018
+        </Footer>
+</Layout>
     );
   }
 }
